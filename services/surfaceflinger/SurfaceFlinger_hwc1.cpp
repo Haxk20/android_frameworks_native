135b17b23fda511f7049132f56f2705108348831
Shilin Victor <chrono.monochrome@gmail.com>
2018-09-08 10:11:15 +0300

    hwc1: port "SF: Fix mGraphicBufferProducerList"
    Change-Id: Ife741c9698b70fe905e34715a15ffa1b0d12fbba

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -688,7 +688,7 @@ bool SurfaceFlinger::authenticateSurfaceTexture(
 bool SurfaceFlinger::authenticateSurfaceTextureLocked(
         const sp<IGraphicBufferProducer>& bufferProducer) const {
     sp<IBinder> surfaceTextureBinder(IInterface::asBinder(bufferProducer));
-    return mGraphicBufferProducerList.indexOf(surfaceTextureBinder) >= 0;
+    return mGraphicBufferProducerList.count(surfaceTextureBinder.get()) > 0;
 }
 
 status_t SurfaceFlinger::getSupportedFrameTimestamps(
@@ -2394,7 +2394,9 @@ status_t SurfaceFlinger::addClientLayer(const sp<Client>& client,
             parent->addChild(lbc);
         }
 
-        mGraphicBufferProducerList.add(IInterface::asBinder(gbc));
+        mGraphicBufferProducerList.insert(IInterface::asBinder(gbc).get());
+        LOG_ALWAYS_FATAL_IF(mGraphicBufferProducerList.size() > MAX_LAYERS,
+                            "Suspected IGBP leak");
         mLayersAdded = true;
         mNumLayers++;
     }
