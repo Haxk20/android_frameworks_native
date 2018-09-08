6b7c6460c15d76a8d9548d8be7694a26abf0ec61
Shilin Victor <chrono.monochrome@gmail.com>
2018-09-08 11:43:00 +0300

    Revert "hwc1: port "SF: Cleanup EventControlThread""
    This reverts commit d7e85d1311140e2a48f8b07fd30c453098c74a2c.
    
    Change-Id: Icf77dbf6a848b9b6e6ad3ea44c5ee775ce22b68e

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -33,8 +33,6 @@
 #include <binder/IServiceManager.h>
 #include <binder/PermissionCache.h>
 
-#include <hardware/hwcomposer2.h>
-
 #include <ui/DisplayInfo.h>
 #include <ui/DisplayStatInfo.h>
 
@@ -273,15 +271,6 @@ static sp<ISurfaceComposerClient> initClient(const sp<Client>& client) {
     }
     return nullptr;
 }
-
-#if 0
-void SurfaceFlinger::setVsyncEnabled(int disp, int enabled) {
-    ATRACE_CALL();
-    Mutex::Autolock lock(mStateLock);
-    getHwComposer().setVsyncEnabled(disp,
-            enabled ? HWC2::Vsync::Enable : HWC2::Vsync::Disable);
-}
-#endif
 
 sp<ISurfaceComposerClient> SurfaceFlinger::createConnection() {
     return initClient(new Client(this));
@@ -626,11 +615,8 @@ void SurfaceFlinger::init() {
     // (which may happens before we render something)
     getDefaultDisplayDeviceLocked()->makeCurrent();
 
-    mEventControlThread = std::make_unique<EventControlThread>([this](bool enabled) {
-        (void)this;
-        (void)enabled;
-        //setVsyncEnabled(HWC_DISPLAY_PRIMARY, enabled);
-    });
+    mEventControlThread = new EventControlThread(this);
+    mEventControlThread->run("EventControl", PRIORITY_URGENT_DISPLAY);
 
     // set a fake vsync period if there is no HWComposer
     if (getBE().mHwc->initCheck() != NO_ERROR) {
