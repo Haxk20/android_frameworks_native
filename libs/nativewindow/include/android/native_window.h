bb3fe80c1f24ad9bcf4948a2a329688b3f211649
Shilin Victor <chrono.monochrome@gmail.com>
2018-09-18 15:32:18 +0300

    libnatiwindow: reset to lineage-15.1
    Change-Id: I7ec4a8c8f24d4b9069bdd96b30a76721622bd872

--- libs/nativewindow/include/android/native_window.h
+++ libs/nativewindow/include/android/native_window.h
@@ -29,7 +29,6 @@
 
 #include <sys/cdefs.h>
 
-#include <android/data_space.h>
 #include <android/hardware_buffer.h>
 #include <android/rect.h>
 
@@ -85,23 +84,23 @@ typedef struct ANativeWindow ANativeWindow;
  * A pointer can be obtained using {@link ANativeWindow_lock()}.
  */
 typedef struct ANativeWindow_Buffer {
-    /// The number of pixels that are shown horizontally.
+    // The number of pixels that are show horizontally.
     int32_t width;
 
-    /// The number of pixels that are shown vertically.
+    // The number of pixels that are shown vertically.
     int32_t height;
 
-    /// The number of *pixels* that a line in the buffer takes in
-    /// memory. This may be >= width.
+    // The number of *pixels* that a line in the buffer takes in
+    // memory. This may be >= width.
     int32_t stride;
 
-    /// The format of the buffer. One of AHARDWAREBUFFER_FORMAT_*
+    // The format of the buffer. One of AHARDWAREBUFFER_FORMAT_*
     int32_t format;
 
-    /// The actual bits.
+    // The actual bits.
     void* bits;
 
-    /// Do not touch.
+    // Do not touch.
     uint32_t reserved[6];
 } ANativeWindow_Buffer;
 
@@ -109,33 +108,33 @@ typedef struct ANativeWindow_Buffer {
  * Acquire a reference on the given {@link ANativeWindow} object. This prevents the object
  * from being deleted until the reference is removed.
  */
-void ANativeWindow_acquire(ANativeWindow* window);
+void ANativeWindow_acquire(ANativeWindow* window) __INTRODUCED_IN(26);
 
 /**
  * Remove a reference that was previously acquired with {@link ANativeWindow_acquire()}.
  */
-void ANativeWindow_release(ANativeWindow* window);
+void ANativeWindow_release(ANativeWindow* window) __INTRODUCED_IN(26);
 
 /**
  * Return the current width in pixels of the window surface.
  *
  * \return negative value on error.
  */
-int32_t ANativeWindow_getWidth(ANativeWindow* window);
+int32_t ANativeWindow_getWidth(ANativeWindow* window) __INTRODUCED_IN(26);
 
 /**
  * Return the current height in pixels of the window surface.
  *
  * \return a negative value on error.
  */
-int32_t ANativeWindow_getHeight(ANativeWindow* window);
+int32_t ANativeWindow_getHeight(ANativeWindow* window) __INTRODUCED_IN(26);
 
 /**
  * Return the current pixel format (AHARDWAREBUFFER_FORMAT_*) of the window surface.
  *
  * \return a negative value on error.
  */
-int32_t ANativeWindow_getFormat(ANativeWindow* window);
+int32_t ANativeWindow_getFormat(ANativeWindow* window) __INTRODUCED_IN(26);
 
 /**
  * Change the format and size of the window buffers.
@@ -155,7 +154,7 @@ int32_t ANativeWindow_getFormat(ANativeWindow* window);
  * \return 0 for success, or a negative value on error.
  */
 int32_t ANativeWindow_setBuffersGeometry(ANativeWindow* window,
-        int32_t width, int32_t height, int32_t format);
+        int32_t width, int32_t height, int32_t format) __INTRODUCED_IN(26);
 
 /**
  * Lock the window's next drawing surface for writing.
@@ -168,7 +167,7 @@ int32_t ANativeWindow_setBuffersGeometry(ANativeWindow* window,
  * \return 0 for success, or a negative value on error.
  */
 int32_t ANativeWindow_lock(ANativeWindow* window, ANativeWindow_Buffer* outBuffer,
-        ARect* inOutDirtyBounds);
+        ARect* inOutDirtyBounds) __INTRODUCED_IN(26);
 
 /**
  * Unlock the window's drawing surface after previously locking it,
@@ -176,9 +175,7 @@ int32_t ANativeWindow_lock(ANativeWindow* window, ANativeWindow_Buffer* outBuffe
  *
  * \return 0 for success, or a negative value on error.
  */
-int32_t ANativeWindow_unlockAndPost(ANativeWindow* window);
-
-#if __ANDROID_API__ >= __ANDROID_API_O__
+int32_t ANativeWindow_unlockAndPost(ANativeWindow* window) __INTRODUCED_IN(26);
 
 /**
  * Set a transform that will be applied to future buffers posted to the window.
@@ -186,36 +183,7 @@ int32_t ANativeWindow_unlockAndPost(ANativeWindow* window);
  * \param transform combination of {@link ANativeWindowTransform} flags
  * \return 0 for success, or -EINVAL if \p transform is invalid
  */
-int32_t ANativeWindow_setBuffersTransform(ANativeWindow* window, int32_t transform);
-
-#endif // __ANDROID_API__ >= __ANDROID_API_O__
-
-#if __ANDROID_API__ >= __ANDROID_API_P__
-
-/**
- * All buffers queued after this call will be associated with the dataSpace
- * parameter specified.
- *
- * dataSpace specifies additional information about the buffer.
- * For example, it can be used to convey the color space of the image data in
- * the buffer, or it can be used to indicate that the buffers contain depth
- * measurement data instead of color images. The default dataSpace is 0,
- * ADATASPACE_UNKNOWN, unless it has been overridden by the producer.
- *
- * \param dataSpace data space of all buffers queued after this call.
- * \return 0 for success, -EINVAL if window is invalid or the dataspace is not
- * supported.
- */
-int32_t ANativeWindow_setBuffersDataSpace(ANativeWindow* window, int32_t dataSpace);
-
-/**
- * Get the dataspace of the buffers in window.
- * \return the dataspace of buffers in window, ADATASPACE_UNKNOWN is returned if
- * dataspace is unknown, or -EINVAL if window is invalid.
- */
-int32_t ANativeWindow_getBuffersDataSpace(ANativeWindow* window);
-
-#endif // __ANDROID_API__ >= __ANDROID_API_P__
+int32_t ANativeWindow_setBuffersTransform(ANativeWindow* window, int32_t transform) __INTRODUCED_IN(26);
 
 #ifdef __cplusplus
 };
